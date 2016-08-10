c1ce375da9fed75b6ba269bb6f2c1a8adbb8aa07
Jorge Saud <jorgelsaud@gmail.com>
2016-08-10 18:43:24 -0400

    fix map2

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,3 +1,22 @@
 function childGeocode(){
-	alert('hola');
-};
+	var geocoder = new google.maps.Geocoder(),
+	address =String('Calle San Juan Bosco,Puerto La Cruz, Edo. Anzoátegui');
+	window.address=address;
+	geocoder.geocode({'address': address}, function(results, status) {
+		if (status === google.maps.GeocoderStatus.OK) {
+			map = new google.maps.Map(document.getElementById('map2'), {
+				center: results[0].geometry.location,
+				zoom: 16
+			});
+			var marker = new google.maps.Marker({
+				map: map,
+				//		icon: image,
+				zoomControl: true,
+				scaleControl: true,
+				position: results[0].geometry.location
+			});
+		} else {
+			alert('Geocode was not successful for the following reason: ' + status);
+		}
+	});
+}
