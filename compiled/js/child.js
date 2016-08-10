5463f5b3c964d5c6861c6ddc01403dddad6c234f
Jorge Saud <jorgelsaud@gmail.com>
2016-08-10 18:51:55 -0400

    fix

--- compiled/js/child.js
+++ compiled/js/child.js
@@ -1,13 +1,13 @@
 function childGeocode(){
 	var geocoder = new google.maps.Geocoder(),
 	address =String('Calle San Juan Bosco, Puerto La Cruz');
-	window.address=address;
 	geocoder.geocode({'address': address}, function(results, status) {
 		if (status === google.maps.GeocoderStatus.OK) {
 			map = new google.maps.Map(document.getElementById('map2'), {
 				center: results[0].geometry.location,
 				zoom: 16
 			});
+			alert('doing');
 			var marker = new google.maps.Marker({
 				map: map,
 				//		icon: image,
