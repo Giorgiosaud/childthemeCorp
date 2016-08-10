9d3c71077a847932a41bd7c5762d0ade9c6af7af
Jorge Saud <jorgelsaud@gmail.com>
2016-08-10 17:59:13 -0400

    fix

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?
-require 'Styles.php';
+// require 'Styles.php';
 function informacion_corporativa_length_gen($length){
 return 60;
 }
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,9 +1,7 @@
 function childGeocode(){
 	var geocoder = new google.maps.Geocoder(),
-		url=location.origin,
-	address =String('calle  San Juan Bosco puerto la cruz,Venezuela');
-	window.address=address;
-	geocoder.geocode({'address': address}, function(results, status) {
+		address =String('calle  San Juan Bosco puerto la cruz,Venezuela');
+		geocoder.geocode({'address': address}, function(results, status) {
 		if (status === google.maps.GeocoderStatus.OK) {
 			map = new google.maps.Map(document.getElementById('map2'), {
 				center: results[0].geometry.location,
